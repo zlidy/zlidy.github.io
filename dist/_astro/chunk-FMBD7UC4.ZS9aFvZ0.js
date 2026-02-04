0854755c19f5c2cfdf426b242cef0e9f6cad337f
zlidy <zl.zhangcurry@gmail.com>
2026-02-04 14:12:04 +0800

    feat: 构建项目生成静态文件 dist，用于 GitHub Pages 部署

---
+++ dist/_astro/chunk-FMBD7UC4.ZS9aFvZ0.js
@@ -0,0 +1,15 @@
+import{_ as e}from"./mermaid.core.C24Pu6zs.js";var l=e(()=>`
+  /* Font Awesome icon styling - consolidated */
+  .label-icon {
+    display: inline-block;
+    height: 1em;
+    overflow: visible;
+    vertical-align: -0.125em;
+  }
+  
+  .node .label-icon path {
+    fill: currentColor;
+    stroke: revert;
+    stroke-width: revert;
+  }
+`,"getIconStyles");export{l as g};
---
+++ dist/_astro/chunk-QZHKN3VN.BQK9vQjd.js
@@ -0,0 +1,1 @@
+import{_ as i}from"./mermaid.core.C24Pu6zs.js";var r=class{constructor(t){this.init=t,this.records=this.init()}static{i(this,"ImperativeState")}reset(){this.records=this.init()}};export{r as I};
